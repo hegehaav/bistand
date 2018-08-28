72482f08eeae450724406037a7f67434338ba496
hegehaav <hegehaav@stud.ntnu.no>
2018-08-28 13:35:13 +0200

    add avaiable info before imball

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import '../styles/Header.css';
+
+class Header extends Component {
+  render() {
+    return (
+      <div className="Header">
+        <header className="header">
+          <h1 className="Header-title">BISTAND</h1>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default Header;
---
+++ src/components/ReleaseContent.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import '../styles/ReleaseContent.css';
+
+class ReleaseContent extends Component {
+  render() {
+    return (
+      <h1 className="content">
+        Kommer 1. september
+      </h1>
+    );
+  }
+}
+
+export default ReleaseContent;
