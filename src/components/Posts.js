5157d986c9203b0077d179095f6d5450edb0d9ca
hegehaav <hegehaav@stud.ntnu.no>
2018-08-29 14:56:56 +0200

    add chat image, update dates

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -5,8 +5,8 @@ import Chat from './Chat';
 
 const Posts = ({posts}) => {
   const posts2 = [ //TODO: get from props
-    { id: 2, date:"Mandag 23.06.18 Kl 12.34", type: "CHAT", url: "ChatExample.png"},
-    { id: 1, date:"Mandag 23.06.18 Kl 12.34", type: "CLIP", url: 'https://www.youtube.com/watch?v=4IP_E7efGWE'  }
+    { id: 2, date:"Lørdag 01.08.18 Kl 20.34", type: "CHAT", url: "chat1.png"},
+    { id: 1, date:"Fredag 31.08.18 Kl 12.34", type: "CLIP", url: 'https://www.youtube.com/watch?v=4IP_E7efGWE'  }
 
   ]
   const postList = posts2.map((post) => {
