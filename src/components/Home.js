ce11ccd96c0f8f20a4777fa853519f02eb35401d
hegehaav <hegehaav@stud.ntnu.no>
2018-08-29 14:52:55 +0200

    add 📹 playing

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import '../styles/Home.css';
 import Posts from './Posts.js'
+
 class Home extends Component {
   constructor(props){
     super(props)
@@ -11,8 +12,10 @@ class Home extends Component {
   render() {
 
     return (
+      <div>
+        <Posts posts={this.state.posts}/>
+      </div>
 
-      <Posts posts={this.state.posts}/>
         );
   }
 }
